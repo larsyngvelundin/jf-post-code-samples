727a9fb93719fb0beb4acb1875f3974a3e0987f2
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-08-31 17:25:57 +0200

    Fixed comments and links

--- webhook_to_sql.php
+++ webhook_to_sql.php
@@ -1,17 +1,36 @@
 <?php
+/*** 
+Form used for my tests: https://www.jotform.com/242424133952957
+
+When request is sent from a Jotform, you won't be able to see any of the "echo" responses below.
+You can manually send requests through something like Postman to your webhook server in order to troubleshoot and see these messages.
+***/
+
 // Check for POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Capture input data (you might want to validate and sanitize)
-    $input = file_get_contents('php://input');
+    /***
+    Test the data if it's a valid submission by checking the submission ID.
+    ***/
+    //Note the different name compared to the Thank You POST data
+    //submission_id -> submissionID
+    if (!isset($_POST['submissionID'])) {
+        die("Invalid submission data!");
+    }
 
 
+    /***
+    ## Database Config
 
-
+    NOTE: 
+    Replace the values below with your MYSQL database environment variables 
+    to create a valid connection.
+    ***/
     $db_host = "localhost";
     $db_username = "root";
     $db_password = "";
     $db_name = "us_east_meeting";
     $db_table = "submissions";
+
 
     /***
     Connect to database.
@@ -20,22 +39,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
-    
+
+
     /***
     ## Data to Save
 
     Prepare the data to prevent possible SQL injection vulnerabilities to the database.
 
     NOTE: Add the POST data to save in your database.
-    To view the submission as POST data, see: https://www.jotform.com/help/?p=607527
+    To view the submission as POST data, see: https://www.jotform.com/help/51-how-to-post-submission-data-to-thank-you-page/
     ***/
+    $formID = $mysqli->real_escape_string($_POST['formID']);
+    //Note the difference in the formatting of the data compared to the Thank You POST data
+    //Submission data itself is now within 'rawRequest'
     $submission_data = json_decode($_POST['rawRequest'], true);
+    //The fields now use prefix, in the form of 'q1_' with the number being their Question ID
     $first_name = $mysqli->real_escape_string($submission_data['q3_name']['first']);
     $last_name = $mysqli->real_escape_string($submission_data['q3_name']['last']);
     $email = $mysqli->real_escape_string($submission_data['q4_email']);
     $message = $mysqli->real_escape_string($submission_data['q7_message']);
-    $date = $mysqli->real_escape_string(implode("-", $submission_data['q8_date']));
-    $formID = $mysqli->real_escape_string($_POST['formID']);
 
 
     /***
@@ -45,6 +67,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $result = $mysqli->query("SELECT * FROM $db_table WHERE submission_id = '$sid'");
 
 
+    /***
+    ## Queries to Run
+
+    Perform the test and then UPDATE or INSERT the record
+    depending if the submission is already in the database or not.
+
+    NOTE:
+    Edit the queries below according to your form and database table structure.
+    For more information, see:
+    - https://www.freecodecamp.org/news/the-sql-update-statement-explained/#how-do-you-use-an-update-statement
+    - https://www.freecodecamp.org/news/sql-insert-and-insert-into-statements-with-example-syntax/#how-to-use-insert-into-in-sql
+    ***/
     if ($result->num_rows > 0) {
         /* UPDATE query */
         $result = $mysqli->query("UPDATE $db_table 
@@ -63,19 +97,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             first_name,
             last_name, 
             email, 
-            message,
-            date
+            message
         ) VALUES (
             '$formID',
             '$sid', 
             '$first_name', 
             '$last_name', 
             '$email',
-            '$message',
-            '$date')
+            '$message')
         ");
     }
 
+
+    /***
+    Echo the outcome.
+    ***/
+    if ($result === true) {
+        echo "Success!";
+    } else {
+        echo "SQL error:" . $mysqli->error;
+    }
 } else {
     // Not a POST request, send an error response
     echo "Error: You must send a POST request.";
